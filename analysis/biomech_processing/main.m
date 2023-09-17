b72d258275cf384f9dca807f426aaae8a180619e
Jesse Charlton <57236497+jchar32@users.noreply.github.com>
2023-09-17 15:34:16 -0700

    initial commit

---
+++ analysis/biomech_processing/main.m
@@ -0,0 +1,8 @@
+% Processing for PAINGAIT study
+
+% 1. ingest trial numbers for participants that correspond to each walking trial and condition
+% 2. identify gait cycles to use for analysis
+% 3. calculate gait outcomes
+% 4. compile waveforms and time normalize
+
+
