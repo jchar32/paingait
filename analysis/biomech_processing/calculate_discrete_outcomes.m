ae72243758a2779db4b835340e2bb67c2b632f4d
Jesse Charlton <57236497+jchar32@users.noreply.github.com>
2023-09-22 08:32:26 -0700

    hip outcomes added

--- analysis/biomech_processing/calculate_discrete_outcomes.m
+++ analysis/biomech_processing/calculate_discrete_outcomes.m
@@ -17,15 +17,68 @@ for c = 1:1%size(condition_names,1)
     for s = 1:size(events.(condition_names{c}).l.ON)-1
         discrete_data.(condition_names{c}).knee.l = knee_outcomes(all_data.(condition_names{c}), events.(condition_names{c}), s, "L", sample_rate);
         discrete_data.(condition_names{c}).ankle.l = ankle_outcomes(all_data.(condition_names{c}), events.(condition_names{c}), s, "L", sample_rate);
+        discrete_data.(condition_names{c}).hip.l = hip_outcomes(all_data.(condition_names{c}), events.(condition_names{c}), s, "L", sample_rate);
     end
     
     for s = 1:size(events.(condition_names{c}).r.ON)-1
         discrete_data.(condition_names{c}).knee.r = knee_outcomes(all_data.(condition_names{c}), events.(condition_names{c}), s, "R", sample_rate);
         discrete_data.(condition_names{c}).ankle.r = ankle_outcomes(all_data.(condition_names{c}), events.(condition_names{c}), s, "R", sample_rate);
+        discrete_data.(condition_names{c}).hip.r = hip_outcomes(all_data.(condition_names{c}), events.(condition_names{c}), s, "R", sample_rate);
     end
     
 
 end
+
+
+end
+
+function [out] = hip_outcomes(data, events, stride, side, sample_rate)
+[ON, OFF, ON_next, stance_frames, stride_frames] = unpack_events(events, stride, side, sample_rate);
+
+HA = data.(side + "HA"){1,1};
+% Sagittal
+[out.peak_fa,      out.time.peak_fa]           = max(HA(ON:ON+round(stance_frames*0.5), 1));
+[out.peak_ea,      out.time.peak_ea]           = min(HA(ON:OFF, 1)); 
+[out.peak_fa_swing,out.time.peak_fa_swing]     = max(HA(ON:ON_next, 1));
+[out.fa_hs,    out.time.fa_hs]         = max(HA(ON, 1));
+[out.fa_to,    out.time.fa_to]         = max(HA(OFF, 1));
+
+% Frontal
+[out.peak_aa,      out.time.peak_aa]           = min(HA(ON:OFF, 2));
+[out.mean_aa]           = mean(HA(ON:OFF, 2));
+[out.peak_aa_swing,out.time.peak_aa_swing]     = max(HA(ON:ON_next, 2));
+[out.aa_hs,    out.time.aa_hs]         = max(HA(ON, 2));
+[out.aa_to,    out.time.aa_to]         = max(HA(OFF, 2));
+
+% angular velocity
+[ha_deriv] = central_difference(HA(:,:), sample_rate.mocap);
+
+% Kinetics
+HM = data.(side + "HM"){1,1};
+% Sagittal
+[out.peak_fm, out.time.peak_fm] = min(HM(ON:OFF, 1));
+[out.peak_em, out.time.peak_em] = max(HM(ON:OFF, 1));
+positive_idx = find(HM(ON:OFF,1) >= 0) + ON;
+neg_idx = find(HM(ON:OFF,1) <= 0) + ON;
+[out.impulse_em]                    = trapz(HM(positive_idx,1))*(1/sample_rate.mocap);
+[out.impulse_fm]                    = trapz(HM(neg_idx,1))*(1/sample_rate.mocap);
+
+% Frontal
+[out.peak_am, out.time.peak_am]     = max(HM(ON:OFF,2));
+[out.peak_am1, out.time.peak_am1]   = max(HM(ON:ON+round(stance_frames*0.5), 2));
+[out.peak_am2, out.time.peak_am2]   = max(HM(ON+round(stance_frames*0.5):OFF, 2)); out.time.peak_am2 + ON+round(stance_frames*0.5);
+[out.unload_am, out.time.unload_am]     = min(HM(out.time.peak_am1:out.time.peak_am2, 2)); out.time.unload_am + out.time.peak_am1;
+positive_idx = find(HM(ON:OFF,2) >= 0) + ON;
+[out.impulse_am]                    = trapz(HM(positive_idx,2))*(1/sample_rate.mocap);
+
+% loading rate
+[hm_deriv] = central_difference(HM(:,:), sample_rate.mocap);
+[out.peak_fm_rate, out.time.peak_fm_rate] = max(hm_deriv(ON:ON+round(stance_frames*0.5),1));
+[out.peak_am_rate, out.time.peak_am_rate] = min(hm_deriv(ON:ON+round(stance_frames*0.5),2));
+
+% Composite outcomes
+% per Zeni et al 2009 Clin Biomech (24) - 3% stance to to peak knee flexion angle
+[out.djs_f, out.djs_a] = dynamic_joint_stiffness(hm_deriv, ha_deriv, round(stance_frames*0.03) + ON, out.time.peak_fa+ON);
 
 
 end
@@ -40,10 +93,6 @@ KA = data.(side + "KA"){1,1};
 % Sagittal
 [out.peak_fa,      out.time.peak_fa]           = max(KA(ON:ON+round(stance_frames*0.5), 1));
 [out.peak_ea,      out.time.peak_ea]           = min(KA(out.time.peak_fa:OFF, 1)); out.time.peak_ea + [0, + out.time.peak_fa]; 
-[out.peak_fa_swing,out.time.peak_fa_swing]     = max(KA(ON:ON_next, 1));
-[out.fa_hs,    out.time.fa_hs]         = max(KA(ON, 1));
-[out.fa_to,    out.time.fa_to]         = max(KA(OFF, 1));
-
 [out.peak_fa_swing,out.time.peak_fa_swing]     = max(KA(ON:ON_next, 1));
 [out.fa_hs,    out.time.fa_hs]         = max(KA(ON, 1));
 [out.fa_to,    out.time.fa_to]         = max(KA(OFF, 1));
@@ -72,7 +121,7 @@ positive_idx = find(KM(ON:OFF,1) >= 0) + ON;
 [out.peak_am, out.time.peak_am]     = max(KM(ON:OFF,2));
 [out.peak_am1, out.time.peak_am1]   = max(KM(ON:ON+round(stance_frames*0.5), 2));
 [out.peak_am2, out.time.peak_am2]   = max(KM(ON+round(stance_frames*0.5):OFF, 2)); out.time.peak_am2 + ON+round(stance_frames*0.5);
-[out.peak_am, out.time.peak_am]     = min(KM(out.time.peak_am1:out.time.peak_am2, 2)); out.time.peak_am + out.time.peak_am1;
+[out.unload_am, out.time.unload_am]     = min(KM(out.time.peak_am1:out.time.peak_am2, 2)); out.time.unload_am + out.time.peak_am1;
 positive_idx = find(KM(ON:OFF,2) >= 0) + ON;
 [out.impulse_am]                    = trapz(KM(positive_idx,2))*(1/sample_rate.mocap);
 
@@ -84,7 +133,6 @@ positive_idx = find(KM(ON:OFF,2) >= 0) + ON;
 % Composite outcomes
 % per Zeni et al 2009 Clin Biomech (24) - 3% stance to to peak knee flexion angle
 [out.djs_f, out.djs_a] = dynamic_joint_stiffness(km_deriv, ka_deriv, round(stance_frames*0.03) + ON, out.time.peak_fa+ON);
-
 
 end
 
@@ -126,7 +174,6 @@ end
 
 
 function [events_out] = temporal_outcomes(ON, OFF)
-    
     events_out.stride_time = diff(ON);
     events_out.stride_time(events_out.stride_time > 1.8) = NaN; % replace the swing times that are incorrect due to missing ONs that were removed
     events_out.stance_time = OFF-ON; 
@@ -143,34 +190,31 @@ function [isgood]= is_good_stride(ON, OFF,s)
         isgood=false;
     elseif (OFF(s) - ON(s) < 0)
     end
-
 end
 
 function [dy] = central_difference(y, h)
-% expects y is an n samples by m channels array
-n =size(y,1);
-for i=2:n-1
-    dy(i,:) = (y(i-1,:)+y(i+1,:)) ./2 ./h;
-end
+    % expects y is an n samples by m channels array
+    n =size(y,1);
+    for i=2:n-1
+        dy(i,:) = (y(i-1,:)+y(i+1,:)) ./2 ./h;
+    end
 end
 
 function [ON, OFF, ON_next, stance_frames, stride_frames] = unpack_events(events, stride, side, sample_rate)
-% unpack events
-ON = round(events.(lower(side)).ON(stride) * sample_rate.mocap);
-OFF = round(events.(lower(side)).OFF(stride) * sample_rate.mocap);
-ON_next = round(events.(lower(side)).ON(stride+1) * sample_rate.mocap);
-stance_frames = OFF-ON;
-stride_frames = ON_next-ON;
+    % unpack events
+    ON = round(events.(lower(side)).ON(stride) * sample_rate.mocap);
+    OFF = round(events.(lower(side)).OFF(stride) * sample_rate.mocap);
+    ON_next = round(events.(lower(side)).ON(stride+1) * sample_rate.mocap);
+    stance_frames = OFF-ON;
+    stride_frames = ON_next-ON;
 end
 
 function [djs_f, djs_a] = dynamic_joint_stiffness(moment_deriv, angle_deriv, startframe, endframe)
-% Composite outcomes
-% per Zeni et al 2009 Clin Biomech (24) - 3% stance to to peak knee flexion angle
-
-[fp,~]=polyfit(angle_deriv(startframe:endframe,1), moment_deriv(startframe:endframe,1), 1);
-[ap,~]=polyfit(angle_deriv(startframe:endframe,2), moment_deriv(startframe:endframe,1), 2);
-[djs_f] = fp(1); % slope of line
-[djs_a] = ap(1); 
-
-
+    % Composite outcomes
+    % per Zeni et al 2009 Clin Biomech (24) - 3% stance to to peak knee flexion angle
+    
+    [fp,~]=polyfit(angle_deriv(startframe:endframe,1), moment_deriv(startframe:endframe,1), 1);
+    [ap,~]=polyfit(angle_deriv(startframe:endframe,2), moment_deriv(startframe:endframe,1), 2);
+    [djs_f] = fp(1); % slope of line
+    [djs_a] = ap(1); 
 end
